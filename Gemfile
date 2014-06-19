5d66d1a2ec172c710f97941dcbb771bde5c0ee1a
Andrew Watkins <andrew.watkinz@gmail.com>
2014-06-19 12:02:19 -0600

    colors, time fix, and game loop fix

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'minitest', '~> 5.3.4'
+gem 'term-ansicolor', '~> 1.3.0'
--- lib/cli.rb
+++ lib/cli.rb
@@ -2,6 +2,12 @@ require './lib/pattern_generator'
 require './lib/pattern_matcher'
 require './lib/guess_validator'
 require './lib/game.rb'
+require 'term/ansicolor'
+
+class String
+  include Term::ANSIColor
+end
+
 
 
 class CLI
@@ -15,7 +21,18 @@ class CLI
     @time = Time.new
   end
 
+
   def start
+    puts %q{
+ __   __  _______  _______  _______  _______  ______    __   __  ___   __    _  ______
+|  |_|  ||   _   ||       ||       ||       ||    _ |  |  |_|  ||   | |  |  | ||      |
+|       ||  |_|  ||  _____||_     _||    ___||   | ||  |       ||   | |   |_| ||  _    |
+|       ||       || |_____   |   |  |   |___ |   |_||_ |       ||   | |       || | |   |
+|       ||       ||_____  |  |   |  |    ___||    __  ||       ||   | |  _    || |_|   |
+| ||_|| ||   _   | _____| |  |   |  |   |___ |   |  | || ||_|| ||   | | | |   ||       |
+|_|   |_||__| |__||_______|  |___|  |_______||___|  |_||_|   |_||___| |_|  |__||______|
+    }.yellow
+
     puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
     print "enter command ~> "
     user_input = gets.strip.downcase
@@ -28,23 +45,23 @@ class CLI
         execute
       else
       puts "Sorry, I don't know how to (#{user_input})"
-      CLI.new.start
+      instructions
     end
   end
 
   def instructions
-    puts "I have generated a beginner sequence with four elements made up of: (r)ed,"
-    puts "(g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game."
+    puts "I have generated a beginner sequence with four elements made up of: #{'(r)ed'.red},"
+    puts "#{'(g)reen'.green}, #{'(b)lue'.blue}, and #{'(y)ellow'.yellow}. Use (q)uit at any time to end the game."
     print "Let's (p)lay:"
     user_input = gets.strip.downcase
     case user_input
     when 'q'
-      puts 'Later biatch'
+      puts 'Later Biatch!'
     when 'p'
       execute
     else
       puts "Sorry, I don't know how to (#{user_input})"
-      CLI.new.start
+      instructions
     end
   end
 
@@ -57,21 +74,20 @@ class CLI
       @turns_left -= 1
         if validator.correct_length? == false || validator.correct_letters? == false
           puts 'Your guess must be a length of FOUR and'
-          puts 'be of these colors (r)ed, (y)ellow, (g)reen, (b)lue'
+          puts "be of these colors #{'(r)ed'.red}, #{'(y)ellow'.yellow}, #{'(g)reen'.green}, #{'(b)lue'.blue}"
         else
           matcher = PatternMatcher.new(@user_input, @secret_pattern)
           matcher.content_match
           matcher.position_match
-          puts @secret_pattern
           @content_match  = matcher.output[:correct_content]
           @position_match = matcher.output[:correct_position]
           puts "You have #{@content_match} content matches and #{@position_match} position matches"
           puts "You have taken #{@turns} turn(s)"
-          puts "#{@turns_left} turns left!"
+          puts "#{@turns_left} turn(s) left!"
           if @position_match == 4
             minutes = Time.now.min - @time.min
             seconds = Time.now.sec - @time.sec
-            puts "You won in #{minutes} min and #{seconds} secs!"
+            puts "You won in #{minutes.abs} min and #{seconds.abs} secs!"
           end
           if @turns_left == 0
             puts "You LOOOOOOSE Muahahahah!"
